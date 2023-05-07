7abb656d0525b5d15f6f2f2d3f4a69258e741804
ErsaGun Tosun <fatihetosun+git@gmail.com>
2023-05-07 16:14:09 +0300

    added jar logo

--- src/components/Jar.js
+++ src/components/Jar.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import AddWord from "./AddWord";
 import Question from "./Question";
 
+import Img from "../assets/img.svg";
+
 function Jar({
   show,
   addWordModal,
@@ -31,9 +33,9 @@ function Jar({
   const AddWordData = (data) => {
     const wordsJson = JSON.parse(localStorage.getItem("words"));
     const isHave = wordsJson.find((word) => word.id === data.id);
-    if(isHave){
+    if (isHave) {
       console.log("id error");
-    }else{
+    } else {
       wordsJson.push(data);
       localStorage.setItem("words", JSON.stringify(wordsJson));
       setWords(wordsJson);
@@ -41,9 +43,13 @@ function Jar({
 
   }
 
+  // elinde 100 tane kelime var bu kelimelerin kendilerine özel zorluk seviyeleri, doğru sayıları, yanlış sayıları, gösterilme sayıları var.
+  // bunları nasıl sıralarsın ve nasıl seçersin. 
+
+
   const UpdateWordData = (data) => {
-    for(let i = 0; i < words.length; i++) {
-      if(words[i].id === data.id) {
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].id === data.id) {
         words[i].falseCount = data.falseCount;
         words[i].trueCount = data.trueCount;
         words[i].showCount = data.showCount;
@@ -73,17 +79,21 @@ function Jar({
               UpdateWordData={UpdateWordData}
             />
 
-            <p className="mb-4 text-5xl italic text-neutral-950 dark:text-white">Click and test yourself</p>
-            <div className="flex justify-center">
+            <p className="mb-4 text-5xl italic text-neutral-950 dark:text-white ">Click and test yourself</p>
+            {/* <div className="flex justify-center">
               <div
                 onClick={handleQuestionModal}
                 className="rounded-full bg-neutral-950 dark:bg-white flex justify-center items-center hover:scale-105 cursor-pointer w-72 h-72"
               >
                 <p className="text-white dark:text-neutral-950">{wordCount}</p>
               </div>
-
+            </div> */}
+            <div className="flex justify-center dark:hidden mt-5">
+              <img onClick={handleQuestionModal} className="dark:invisible hover:scale-110 hover:rotate-3 cursor-pointer w-64 h-80" src={Img} />
             </div>
-
+            <div className="hidden dark:flex justify-center mt-5">
+              <img onClick={handleQuestionModal} className="hover:scale-110 hover:rotate-3 invert cursor-pointer w-64 h-80" src={Img} />
+            </div>
           </div>
         </div>
       </main>
